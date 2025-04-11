9b238868df67fb1914e9f4dc1c11f9687e0bd980
Abdullah Saeed <abdullahsaeed6676@gmail.com>
2025-04-12 01:47:13 +0300

    some additions

--- Assets/Scripts/ObjectiveSystem/Objectives/EndActions/ArrestJosh EndAction.cs
+++ Assets/Scripts/ObjectiveSystem/Objectives/EndActions/ArrestJosh EndAction.cs
@@ -1,17 +1,23 @@
+using DG.Tweening;
 using System;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class ArrestJoshEndAction : MonoBehaviour, IObjectiveEndAction
 {
+    [SerializeField] private AudioMixer mixer;
     private Animator _animator;
 
     public event EventHandler OnExecutionEnd;
 
-
+    private AudioSource _audioSource;
+    private float _defaultVolume = 0f;
 
     private void Awake()
     {
-        _animator = GetComponentInChildren<Animator>();
+        _animator = GetComponent<Animator>();
+        _audioSource = GetComponentInChildren<AudioSource>();
     }
 
     public void EndExecute()
@@ -20,5 +26,24 @@ public class ArrestJoshEndAction : MonoBehaviour, IObjectiveEndAction
         GameplayInputManager.Instance.playerControls.Gameplay.Disable();
 
         OnExecutionEnd?.Invoke(this, EventArgs.Empty);
+    }
+
+    public void TeleportToNewsScene()
+    {
+        GameplayInputManager.Instance.playerControls.Gameplay.Enable();
+        mixer.SetFloat("MasterVol", _defaultVolume);
+
+        SceneManager.LoadScene("The News");
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        _audioSource.PlayOneShot(clip);
+    }
+
+    public void MuteAllAudio()
+    {
+        _defaultVolume = PlayerPrefs.GetFloat("MasterVol");
+        mixer.DOSetFloat("MasterVol", -80f, 1);
     }
 }
